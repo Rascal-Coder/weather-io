7cce040e8154a778d1015494c55b3fba8d02e20b
一个小瘪三 <10948399+menon-qiqi@user.noreply.gitee.com>
2024-05-13 00:02:53 +0800

    feat: API Joint debugging

--- assets/js/api.js
+++ assets/js/api.js
@@ -4,3 +4,34 @@
  * @copyright codewithsadee 2023 All rights reserved
  * @author codewithsadee <mohammadsadee24@gmail.com>
  */
+
+'use strict'
+const api_key = '612fe0c2c22681284f5bdf0b1f77bd17'
+
+export const fetchData = (URL, callback) => {
+    fetch(`${URL}&appid=${api_key}`)
+        .then(res => res.json())
+        .then(data => callback(data))
+}
+export const url = {
+    currentWeather(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric&lang=en`
+    },
+    forecast(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric&lang=en`
+    },
+    airPollution(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+    },
+    reverseGeo(lat, lon) {
+        return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+    },
+    /**
+     * 
+     * @param {string} query Search query e.g. : "Chengdu"
+     * @returns 
+     */
+    geo(query) {
+        return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+    }
+}
--- assets/js/route.js
+++ assets/js/route.js
@@ -4,3 +4,45 @@
  * @copyright codewithsadee 2023 All rights reserved
  * @author codewithsadee <mohammadsadee24@gmail.com>
  */
+
+'use strict';
+
+import { updateWeather, error404 } from "./app.js";
+const defaultLocation = "#/weather?lat=30.6598628&lon=104.0633717" //Chengdu
+const currentLocation = function () {
+    window.navigator.geolocation.getCurrentPosition(res => {
+        const { latitude, longitude } = res.coords;
+
+        updateWeather(`lat=${latitude}`, `lon=${longitude}`);
+    }, err => {
+        window.location.hash = defaultLocation;
+    })
+}
+/**
+ * @param {string} query Searched query (lat&lon)
+ */
+const searchLocation = (query) => updateWeather(...query.split("&"));
+// updateWeather("lat=30.6598628",lon=104.0633717");
+
+const routes = new Map([
+    ["/current-location", currentLocation],
+    ["/weather", searchLocation]
+]);
+
+const checkHash = function () {
+    const hash = window.location.hash.slice(1);
+
+    const [route, query] = hash.includes ? hash.split("?") : [hash];
+
+    routes.get(route) ? routes.get(route)(query) : error404();
+}
+
+window.addEventListener("hashchange", checkHash);
+
+window.addEventListener("load", function () {
+    if (!window.location.hash) {
+        window.location.hash = "#/current-location";
+    } else {
+        checkHash();
+    }
+})

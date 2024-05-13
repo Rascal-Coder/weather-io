b2385a4ab7af45120c6c8d02cc2ada88b3bafed8
qiyuhang <qiyuhang@zhixue.com>
2024-05-13 16:21:44 +0800

    chore: 测试iframe通信

--- assets/js/route.js
+++ assets/js/route.js
@@ -10,12 +10,7 @@
 import { updateWeather, error404 } from "./app.js";
 const defaultLocation = "#/weather?lat=30.6598628&lon=104.0633717"; //Chengdu
 const currentLocation = function () {
-  window.addEventListener("message", function (event) {
-    const { latitude, longitude, isIframe } = event.data;
-    if (isIframe) {
-        updateWeather(`lat=${latitude}`, `lon=${longitude}`);
-    } else {
-      window.navigator.geolocation.getCurrentPosition(
+    window.navigator.geolocation.getCurrentPosition(
         (res) => {
           const { latitude, longitude } = res.coords;
           updateWeather(`lat=${latitude}`, `lon=${longitude}`);
@@ -24,8 +19,22 @@ const currentLocation = function () {
           window.location.hash = defaultLocation;
         }
       );
-    }
-  });
+//   window.addEventListener("message", function (event) {
+//     const { latitude, longitude, isIframe } = event.data;
+//     if (isIframe) {
+//         updateWeather(`lat=${latitude}`, `lon=${longitude}`);
+//     } else {
+//       window.navigator.geolocation.getCurrentPosition(
+//         (res) => {
+//           const { latitude, longitude } = res.coords;
+//           updateWeather(`lat=${latitude}`, `lon=${longitude}`);
+//         },
+//         (err) => {
+//           window.location.hash = defaultLocation;
+//         }
+//       );
+//     }
+//   });
 };
 /**
  * @param {string} query Searched query (lat&lon)

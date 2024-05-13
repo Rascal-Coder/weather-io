0b80c989d7fa63cc8ec979aa8b27973e1d2d8454
qiyuhang <qiyuhang@zhixue.com>
2024-05-13 17:39:23 +0800

    chore: 更改UpdateWeather逻辑

--- assets/js/route.js
+++ assets/js/route.js
@@ -10,31 +10,22 @@
 import { updateWeather, error404 } from "./app.js";
 const defaultLocation = "#/weather?lat=30.6598628&lon=104.0633717"; //Chengdu
 const currentLocation = function () {
+  if (localStorage.getItem("iframeQuery")) {
+    const { latitude, longitude } = JSON.parse(
+      localStorage.getItem("iframeQuery")
+    );
+    updateWeather(`lat=${latitude}`, `lon=${longitude}`);
+  } else {
     window.navigator.geolocation.getCurrentPosition(
-        (res) => {
-          const { latitude, longitude } = res.coords;
-          updateWeather(`lat=${latitude}`, `lon=${longitude}`);
-        },
-        (err) => {
-          window.location.hash = defaultLocation;
-        }
-      );
-//   window.addEventListener("message", function (event) {
-//     const { latitude, longitude, isIframe } = event.data;
-//     if (isIframe) {
-//         updateWeather(`lat=${latitude}`, `lon=${longitude}`);
-//     } else {
-//       window.navigator.geolocation.getCurrentPosition(
-//         (res) => {
-//           const { latitude, longitude } = res.coords;
-//           updateWeather(`lat=${latitude}`, `lon=${longitude}`);
-//         },
-//         (err) => {
-//           window.location.hash = defaultLocation;
-//         }
-//       );
-//     }
-//   });
+      (res) => {
+        const { latitude, longitude } = res.coords;
+        updateWeather(`lat=${latitude}`, `lon=${longitude}`);
+      },
+      (err) => {
+        window.location.hash = defaultLocation;
+      }
+    );
+  }
 };
 /**
  * @param {string} query Searched query (lat&lon)
@@ -53,8 +44,23 @@ const checkHash = function () {
   const [route, query] = hash.includes ? hash.split("?") : [hash];
 
   routes.get(route) ? routes.get(route)(query) : error404();
+  const coordinateQuery = queryMap(query);
+  const isIframe =
+    Object.keys(coordinateQuery).includes("isIframe") &&
+    coordinateQuery.isIframe;
+  if (isIframe) {
+    localStorage.setItem("iframeQuery", JSON.stringify(coordinateQuery));
+  }
 };
-
+const queryMap = (query) => {
+  const keyValuePairs = query.split("&");
+  const coordinates = {};
+  keyValuePairs.forEach((pair) => {
+    const [key, value] = pair.split("=");
+    coordinates[key] = value;
+  });
+  return coordinates;
+};
 window.addEventListener("hashchange", checkHash);
 
 window.addEventListener("load", function () {
